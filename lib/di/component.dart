75053ab64d477438b03a144117d8ccfd1c9d089e
宮西洋輔 <miyanishi1212@gmail.com>
2019-09-01 21:56:44 +0900

    feat: それぞれのモジュールをComponentへ追加

--- lib/di/component.dart
+++ lib/di/component.dart
@@ -1,10 +1,15 @@
 import 'package:makasete_choice_cross_platform/action/action_creater/add_member_creator.dart';
+import 'package:makasete_choice_cross_platform/action/action_creater/grouping_creator.dart';
 import 'package:makasete_choice_cross_platform/action/action_creater/init_member_creator.dart';
 import 'package:makasete_choice_cross_platform/action/action_creater/remove_member_creator.dart';
 import 'package:makasete_choice_cross_platform/di/module/dispatcher_module.dart';
 import 'package:makasete_choice_cross_platform/di/module/member_repository_module.dart';
 import 'package:makasete_choice_cross_platform/di/module/module.dart';
+import 'package:makasete_choice_cross_platform/store/group_store.dart';
 import 'package:makasete_choice_cross_platform/store/member_store.dart';
+
+import 'module/grouping_member_repository_module.dart';
+import 'module/grouping_usecase_module.dart';
 
 class Component {
   static final Component _singleton = new Component._internal();
@@ -17,7 +22,9 @@ class Component {
 
   Map<String, Module> _modules = {
     "MemberRepositoryModule": MemberRepositoryModule(),
-    "DispatcherModule": DispatcherModule()
+    "DispatcherModule": DispatcherModule(),
+    "GroupingUsecaseModule": GroupingUsecaseModule(),
+    "GroupingMemberRepositoryModule": GroupingMemberRepositoryModule()
   };
 
   void injectAddMemberCreator(AddMemberCreator addMemberCreator) {
@@ -47,5 +54,21 @@ class Component {
             .provideMemberRepository();
     initMemberCreator.dispatcher =
         (_modules["DispatcherModule"] as DispatcherModule).provideDispatcher();
+  }
+
+  void injectGroupStore(GroupStore groupStore) {
+    groupStore.dispatcher =
+        (_modules["DispatcherModule"] as DispatcherModule).provideDispatcher();
+  }
+
+  void injectGroupingCreator(GroupingCreator groupingCreator) {
+    groupingCreator.repository = (_modules["GroupingMemberRepositoryModule"]
+            as GroupingMemberRepositoryModule)
+        .provideGroupingMemberRepository();
+    groupingCreator.dispatcher =
+        (_modules["DispatcherModule"] as DispatcherModule).provideDispatcher();
+    groupingCreator.usecase =
+        (_modules["GroupingUsecaseModule"] as GroupingUsecaseModule)
+            .provideGroupingUsecase();
   }
 }
